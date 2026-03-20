718ab2ec44f840718b20ff0cccb15c569e0eab97
Paule-Iris Djeni <pauleirisdgo@gmail.com>
2026-03-20 02:31:09 +0100

    modify supabase.js by importing create client and exporting const supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,3 +1,7 @@
-const SUPABASE_URL  = "https://afbgcdasawgfjqrezrak.supabase.co";
 
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_URL  = "https://afbgcdasawgfjqrezrak.supabase.co";
 const SUPABASE_ANON = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmYmdjZGFzYXdnZmpxcmV6cmFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5NjExNTQsImV4cCI6MjA4OTUzNzE1NH0.BbTNl1M12D26dcjT3B5ZnZJ7GthuimGUB-iHrU6Jfvw";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON);
