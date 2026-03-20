710287bf8aa3bb726d33215bf024ac9f4b698ffe
Paule-Iris Djeni <pauleirisdgo@gmail.com>
2026-03-20 02:28:07 +0100

    added exact anon key

--- src/supabase.js
+++ src/supabase.js
@@ -1,3 +1,3 @@
 const SUPABASE_URL  = "https://afbgcdasawgfjqrezrak.supabase.co";
 
-const SUPABASE_ANON = "Psb_publishable_DpBKqQ8XFArJf4geeOf-vA_a5MfX-gvN_KEY_HERE";
+const SUPABASE_ANON = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmYmdjZGFzYXdnZmpxcmV6cmFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5NjExNTQsImV4cCI6MjA4OTUzNzE1NH0.BbTNl1M12D26dcjT3B5ZnZJ7GthuimGUB-iHrU6Jfvw";
